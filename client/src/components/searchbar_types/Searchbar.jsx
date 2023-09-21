ce3acd8c985330b7bdc501864b7703945d5d9cf9
Pau Fàbrega <paufabregaroda@gmail.com>
2023-09-21 10:39:07 +0200

    fix: searchbar changes

--- client/src/components/searchbar_types/Searchbar.jsx
+++ client/src/components/searchbar_types/Searchbar.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 import { getTypes } from '../../services/apiType';
-import { postDrink } from '../../services/apiDrink'
+import { postDrink } from '../../services/apiDrink';
 
 function TypeSearch({ onTypeSelect }) {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const handleSearch = async () => {
+    if (searchQuery.trim() === '') {
+      // Don't make the API call if the search query is empty
+      setSearchResults([]);
+      return;
+    }
+
     try {
       const types = await getTypes(searchQuery);
-      setSearchResults(types);
+      console.log(types);
+
+      const filteredTypes = types.filter((type) =>
+        type.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setSearchResults(filteredTypes);
     } catch (error) {
       console.error('Error fetching search results:', error);
     }
+  };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    onTypeSelect(option);
+    setSearchQuery(option.name); // Set the input value to the selected option
+    setSearchResults([]); // Hide the dropdown
   };
 
   return (
@@ -21,20 +40,30 @@ function TypeSearch({ onTypeSelect }) {
         type="text"
         placeholder="Search for a drink"
         value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+          handleSearch();
+        }}
       />
-      <button onClick={handleSearch}>Search</button>
-
-      <ul>
-        {searchResults.map((type) => (
-          <li key={type._id} onClick={() => onTypeSelect(type)}>
-            {type.name}
-          </li>
-        ))}
-      </ul>
+      {searchResults.length > 0 && (
+        <div className="dropdown">
+          {searchResults.map((option) => (
+            <div
+              key={option._id}
+              onClick={() => handleOptionSelect(option)}
+              className="dropdown-option"
+            >
+              {option.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
+
+// Rest of your components and code...
+
 
 function DrinkForm({ selectedType, onSubmit }) {
   const [numConsumptions, setNumConsumptions] = useState(1);
@@ -49,6 +78,7 @@ function DrinkForm({ selectedType, onSubmit }) {
 
       // Make the API call to submit the new drink
       await postDrink(newDrink);
+      console.log(newDrink);
     } catch (error) {
       console.error('Error submitting new drink:', error);
     }
